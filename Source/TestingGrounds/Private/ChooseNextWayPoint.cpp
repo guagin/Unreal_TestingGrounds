ed37a62b46299833c2668a7de05c4a02d7155c27
Ricky <guagin0972@gmail.com>
2019-02-13 21:41:30 +0800

    add C++ class ChooseWayPoint.
    print some log in it.

---
+++ Source/TestingGrounds/Private/ChooseNextWayPoint.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWayPoint.h"
+
+EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	UE_LOG(LogTemp, Warning, TEXT("Hey mother fucker"));
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/TestingGrounds/Public/ChooseNextWayPoint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWayPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWayPoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
+
